bf58d994fd68d92ab6fe2dbca16f9e7d07d97bd7
dugurca <dugurca@gmail.com>
2016-11-01 22:18:02 +0300

    init

---
+++ Assets/Project/Scripts/Generate.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+namespace Assets.Project.Scripts
+{
+    public class Generate : MonoBehaviour
+    {
+        void Start ()
+        {
+            GenerateCubeArt();
+        }
+
+        void GenerateCubeArt()
+        {
+            const int n = 25000;
+            const float spread = n/25f;
+            var lst = new List<GameObject>();
+            for (int i = 0; i < n; i++)
+            {
+                lst.Add(CreateCube(spread, 10));
+            }
+            Application.CaptureScreenshot(DateTime.Now.Ticks + ".png", 4);
+
+            return;
+            foreach (var go in lst)
+            {
+                Destroy(go);
+            }
+        }
+
+        GameObject CreateCube(float spread, float scale)
+        {
+            var cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
+            cube.transform.position = Random.insideUnitSphere * spread;
+            cube.transform.rotation = Random.rotation;
+            cube.transform.localScale = Vector3.one*scale;
+            cube.GetComponent<Renderer>().material.color = Random.ColorHSV(0, 1);
+            return cube;
+        }
+    }
+}
